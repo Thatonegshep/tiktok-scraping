22ad839bca21ffbe78c1d897e7c83c15b373f7fe
Thatonegshep <tflores@mymail.tstc.edu>
2025-05-31 12:41:50 -0700

    Update device_gen.py

--- utils/device_gen.py
+++ utils/device_gen.py
@@ -1,214 +1,52 @@
-import random, json, time, hashlib, requests, curlify, base64, sys, colorama, datetime, urllib.parse, threading, uuid, binascii, os
-
-from urllib.parse import *
-from utils.extra.constants import *
+_e='armeabi-v7a'
+_d='offset'
+_c='googleplay'
+_b='os_version'
+_a='channel'
+_Z='Keep-Alive'
+_Y='login=0;ct=0'
+_X='user-agent'
+_W='connection'
+_V='x-tt-dm-status'
+_U='x-ss-req-ticket'
+_T='accept-encoding'
+_S='wifi'
+_R='dpi'
+_Q='cdid'
+_P='timezone_name'
+_O='update_version_code'
+_N='manifest_version_code'
+_M='openudid'
+_L='os_api'
+_K='language'
+_J='X-Khronos'
+_I='X-Gorgon'
+_H=None
+_G='region'
+_F='resolution'
+_E='device_brand'
+_D='aid'
+_C='device_model'
+_B='version_code'
+_A='os'
+import random,json,time,hashlib,requests,curlify,base64,sys,colorama,datetime,urllib.parse,threading,uuid,binascii,os
+from urllib.parse import*
+from utils.extra.constants import*
 from utils.extra.algorithms.ttencrypt import TTEncrypt
 from utils.extra.algorithms.xgorgon import Xgorgon
 from utils.extra.device import Device
-
-# proxies = {
-#     "http": f"http://xtekky:ololol@geo.iproyal.com:22323",
-#     "https": f"http://xtekky:ololol@geo.iproyal.com:22323",
-# }
-
-
 class Applog:
-    def __init__(self, device: dict, proxy):
-        self.__device = device
-        self.__host = "log-va.tiktokv.com"
-        self.proxies = {
-            "http": f"http://{proxy}",
-            "https": f"http://{proxy}",
-        }
-
-    def __headers(self, params: str, payload: (str or bool) = None) -> dict:
-        sig = Xgorgon().calculate(params, payload, None)
-
-        headers = {
-            "x-ss-stub": str(hashlib.md5(str(payload).encode()).hexdigest()).upper(),
-            "accept-encoding": "gzip",
-            "passport-sdk-version": "19",
-            "sdk-version": "2",
-            "x-ss-req-ticket": str(int(time.time())) + "000",
-            "x-tt-dm-status": "login=0;ct=0",
-            "host": self.__host,
-            "connection": "Keep-Alive",
-            "content-type": "application/octet-stream",
-            "user-agent": (
-                f"com.zhiliaoapp.musically/{application['version_code']} "
-                + f"(Linux; U; Android {self.__device['os']}; pt_BR; {self.__device['device_model']}; "
-                + f"Build/{self.__device['build']}; "
-                + "Cronet/TTNetVersion:5f9640e3 2021-04-21 QuicVersion:47946d2a 2020-10-14)"
-            ),
-            "X-Gorgon": sig["X-Gorgon"],
-            "X-Khronos": str(sig["X-Khronos"]),
-        }
-
-        return headers
-
-    def __params(self) -> str:
-        __base_params = {
-            "ac": "wifi",
-            "channel": "googleplay",
-            "aid": application["aid"],
-            "app_name": "musical_ly",
-            "version_code": application["version_code"],
-            "version_name": application["version_name"],
-            "device_platform": "android",
-            "ab_version": application["ab_version"],
-            "ssmix": "a",
-            "device_type": self.__device["device_model"],
-            "device_brand": self.__device["device_brand"],
-            "language": self.__device["language"],
-            "os_api": self.__device["os_api"],
-            "os_version": self.__device["os"],
-            "openudid": self.__device["openudid"],
-            "manifest_version_code": application["manifest_version_code"],
-            "resolution": str(self.__device["resolution"]).split("x")[1]
-            + "*"
-            + str(self.__device["resolution"]).split("x")[0],
-            "dpi": self.__device["dpi"],
-            "update_version_code": application["update_version_code"],
-            "_rticket": round(time.time() * 1000),
-            "app_type": "normal",
-            "sys_region": self.__device["sys_region"],
-            "timezone_name": self.__device["timezone_name"],
-            "app_language": self.__device["app_language"],
-            "ac2": "wifi",
-            "uoo": "0",
-            "op_region": self.__device["op_region"],
-            "timezone_offset": self.__device["offset"],
-            "build_number": application["build_number"],
-            "locale": self.__device["locale"],
-            "region": self.__device["region"],
-            "ts": int(time.time()),
-            "cdid": self.__device["cdid"],
-            "cpu_support64": "true",
-            "host_abi": "armeabi-v7a",
-        }
-
-        return urlencode(__base_params)
-
-    def __payload(self):
-
-        payload = {
-            "magic_tag": "ss_app_log",
-            "header": {
-                "display_name": "TikTok",
-                "update_version_code": application["update_version_code"],
-                "manifest_version_code": application["manifest_version_code"],
-                "app_version_minor": "",
-                "aid": application["aid"],
-                "channel": "googleplay",
-                "package": "com.zhiliaoapp.musically",
-                "app_version": application["app_version"],
-                "version_code": application["version_code"],
-                "sdk_version": "2.12.1-rc.17",
-                "sdk_target_version": 29,
-                "git_hash": application["git_hash"],
-                "os": "Android",
-                "os_version": str(self.__device["os"]),
-                "os_api": self.__device["os_api"],
-                "device_model": self.__device["device_model"],
-                "device_brand": self.__device["device_brand"],
-                "device_manufacturer": self.__device["device_brand"],
-                "cpu_abi": "armeabi-v7a",
-                "release_build": application["release_build"],
-                "density_dpi": self.__device["dpi"],
-                "display_density": self.__device["display_density"],
-                "resolution": self.__device["resolution"],
-                "language": self.__device["language"],
-                "timezone": self.__device["timezone"],
-                "access": "wifi",
-                "not_request_sender": 0,
-                "rom": self.__device["rom"],
-                "rom_version": self.__device["rom_version"],
-                "cdid": self.__device["cdid"],
-                "sig_hash": application["sig_hash"],
-                "gaid_limited": 0,
-                "google_aid": self.__device["google_aid"],
-                "openudid": self.__device["openudid"],
-                "clientudid": self.__device["clientudid"],
-                "region": self.__device["region"],
-                "tz_name": f"{self.__device['timezone_name'].split('/')[0]}\/{self.__device['timezone_name'].split('/')[1]}",
-                "tz_offset": self.__device["offset"],
-                "req_id": self.__device["req_id"],
-                "custom": {
-                    "is_kids_mode": 0,
-                    "filter_warn": 0,
-                    "web_ua": f"Dalvik\/2.1.0 (Linux; U; Android {self.__device['os']}; {self.__device['device_model']} Build\/{self.__device['build']})",
-                    "user_period": 0,
-                    "user_mode": -1,
-                },
-                "apk_first_install_time": self.__device["install_time"],
-                "is_system_app": 0,
-                "sdk_flavor": "global",
-            },
-            "_gen_time": round(time.time() * 1000),
-        }
-        return payload
-
-    @staticmethod
-    def __tt_encryption(data: dict) -> str:
-        ttencrypt = TTEncrypt()
-        data_formated = json.dumps(data).replace(" ", "")
-        return ttencrypt.encrypt(data_formated)
-
-    def register_device(self):
-        params = self.__params()
-        payload = self.__payload()
-
-        r = requests.post(
-            url=("http://" + self.__host + "/service/2/device_register/?" + params),
-            headers=self.__headers(params),
-            data=bytes.fromhex(self.__tt_encryption(payload)),
-            proxies=self.proxies,
-        )
-
-        if r.json()["device_id"] == 0 or r.json()["device_id"] == "0":
-            self.register_device()
-
-        if r.ok:
-            print("registered....")
-
-        return r.json()["device_id"], r.json()["install_id"]
-
-
+	def __init__(self,device,proxy):self.__device=device;self.__host='log-va.tiktokv.com';self.proxies={'http':f"http://{proxy}",'https':f"http://{proxy}"}
+	def __headers(self,params,payload=_H):sig=Xgorgon().calculate(params,payload,_H);headers={'x-ss-stub':str(hashlib.md5(str(payload).encode()).hexdigest()).upper(),_T:'gzip','passport-sdk-version':'19','sdk-version':'2',_U:str(int(time.time()))+'000',_V:_Y,'host':self.__host,_W:_Z,'content-type':'application/octet-stream',_X:f"com.zhiliaoapp.musically/{application[_B]} "+f"(Linux; U; Android {self.__device[_A]}; pt_BR; {self.__device[_C]}; "+f"Build/{self.__device["build"]}; "+'Cronet/TTNetVersion:5f9640e3 2021-04-21 QuicVersion:47946d2a 2020-10-14)',_I:sig[_I],_J:str(sig[_J])};return headers
+	def __params(self):G='locale';F='build_number';E='op_region';D='app_language';C='sys_region';B='ab_version';A='version_name';__base_params={'ac':_S,_a:_c,_D:application[_D],'app_name':'musical_ly',_B:application[_B],A:application[A],'device_platform':'android',B:application[B],'ssmix':'a','device_type':self.__device[_C],_E:self.__device[_E],_K:self.__device[_K],_L:self.__device[_L],_b:self.__device[_A],_M:self.__device[_M],_N:application[_N],_F:str(self.__device[_F]).split('x')[1]+'*'+str(self.__device[_F]).split('x')[0],_R:self.__device[_R],_O:application[_O],'_rticket':round(time.time()*1000),'app_type':'normal',C:self.__device[C],_P:self.__device[_P],D:self.__device[D],'ac2':_S,'uoo':'0',E:self.__device[E],'timezone_offset':self.__device[_d],F:application[F],G:self.__device[G],_G:self.__device[_G],'ts':int(time.time()),_Q:self.__device[_Q],'cpu_support64':'true','host_abi':_e};return urlencode(__base_params)
+	def __payload(self):K='req_id';J='clientudid';I='google_aid';H='sig_hash';G='rom_version';F='rom';E='timezone';D='display_density';C='release_build';B='git_hash';A='app_version';payload={'magic_tag':'ss_app_log','header':{'display_name':'TikTok',_O:application[_O],_N:application[_N],'app_version_minor':'',_D:application[_D],_a:_c,'package':'com.zhiliaoapp.musically',A:application[A],_B:application[_B],'sdk_version':'2.12.1-rc.17','sdk_target_version':29,B:application[B],_A:'Android',_b:str(self.__device[_A]),_L:self.__device[_L],_C:self.__device[_C],_E:self.__device[_E],'device_manufacturer':self.__device[_E],'cpu_abi':_e,C:application[C],'density_dpi':self.__device[_R],D:self.__device[D],_F:self.__device[_F],_K:self.__device[_K],E:self.__device[E],'access':_S,'not_request_sender':0,F:self.__device[F],G:self.__device[G],_Q:self.__device[_Q],H:application[H],'gaid_limited':0,I:self.__device[I],_M:self.__device[_M],J:self.__device[J],_G:self.__device[_G],'tz_name':f"{self.__device[_P].split("/")[0]}\\/{self.__device[_P].split("/")[1]}",'tz_offset':self.__device[_d],K:self.__device[K],'custom':{'is_kids_mode':0,'filter_warn':0,'web_ua':f"Dalvik\\/2.1.0 (Linux; U; Android {self.__device[_A]}; {self.__device[_C]} Build\\/{self.__device["build"]})",'user_period':0,'user_mode':-1},'apk_first_install_time':self.__device['install_time'],'is_system_app':0,'sdk_flavor':'global'},'_gen_time':round(time.time()*1000)};return payload
+	@staticmethod
+	def __tt_encryption(data):ttencrypt=TTEncrypt();data_formated=json.dumps(data).replace(' ','');return ttencrypt.encrypt(data_formated)
+	def register_device(self):
+		A='device_id';params=self.__params();payload=self.__payload();r=requests.post(url='http://'+self.__host+'/service/2/device_register/?'+params,headers=self.__headers(params),data=bytes.fromhex(self.__tt_encryption(payload)),proxies=self.proxies)
+		if r.json()[A]==0 or r.json()[A]=='0':self.register_device()
+		if r.ok:print('registered....')
+		return r.json()[A],r.json()['install_id']
 class Xlog:
-    def __init__(self, __device_id, proxy):
-        self.__device_id = __device_id
-        self.proxies = {
-            "http": f"http://{proxy}",
-            "https": f"http://{proxy}",
-        }
-
-    def bypass(self):
-
-        params = urlencode(
-            {
-                "os": "0",
-                "ver": "0.6.11.29.19-MT",
-                "m": "2",
-                "app_ver": "19.1.3",
-                "region": "en_US",
-                "aid": "1233",
-                "did": self.__device_id,
-            }
-        )
-        sig = Xgorgon().calculate(params, None, None)
-
-        headers = {
-            "accept-encoding": "gzip",
-            "cookie": "sessionid=",
-            "x-ss-req-ticket": str("".join(str(time.time()).split(".")))[:13],
-            "x-tt-dm-status": "login=0;ct=0",
-            "X-Gorgon": sig["X-Gorgon"],
-            "X-Khronos": str(sig["X-Khronos"]),
-            "host": "xlog-va.tiktokv.com",
-            "connection": "Keep-Alive",
-            "user-agent": "okhttp/3.10.0.1",
-        }
-
-        url = "https://xlog-va.tiktokv.com/v2/s/?" + params
-
-        response = requests.get(url, headers=headers, proxies=self.proxies)
+	def __init__(self,__device_id,proxy):self.__device_id=__device_id;self.proxies={'http':f"http://{proxy}",'https':f"http://{proxy}"}
+	def bypass(self):params=urlencode({_A:'0','ver':'0.6.11.29.19-MT','m':'2','app_ver':'19.1.3',_G:'en_US',_D:'1233','did':self.__device_id});sig=Xgorgon().calculate(params,_H,_H);headers={_T:'gzip','cookie':'sessionid=',_U:str(''.join(str(time.time()).split('.')))[:13],_V:_Y,_I:sig[_I],_J:str(sig[_J]),'host':'xlog-va.tiktokv.com',_W:_Z,_X:'okhttp/3.10.0.1'};url='https://xlog-va.tiktokv.com/v2/s/?'+params;response=requests.get(url,headers=headers,proxies=self.proxies)
